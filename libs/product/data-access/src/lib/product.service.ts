89bd678df9311ee2c638e7d3b88c91b9eae30537
AJ Ruth Sumandang <sumandang.ajruth@gmail.com>
2024-07-01 03:19:15 +0800

    feat: add shared dto with product type

--- libs/product/data-access/src/lib/product.service.ts
+++ libs/product/data-access/src/lib/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import * as products from './products.json';
 import { of } from 'rxjs';
+import { Product } from '@product/shared-dto';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,6 @@ export class ProductService {
   private readonly _products = products;
 
   getProducts() {
-    return of(this._products);
+    return of<Product[]>(this._products);
   }
 }
--- libs/product/feature-product-home/src/lib/product-home.page.ts
+++ libs/product/feature-product-home/src/lib/product-home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
@@ -11,7 +11,9 @@ import {
   IonSearchbar,
   type SearchbarCustomEvent,
 } from '@ionic/angular/standalone';
+import { ProductService } from '@product/product-data-access';
 import { ProductCardComponent } from '@product/product-ui/product-card';
+import type { Product } from '@product/shared-dto';
 
 @Component({
   selector: 'pc-product-home',
@@ -33,6 +35,11 @@ import { ProductCardComponent } from '@product/product-ui/product-card';
 })
 export class ProductHomePage {
   readonly debounceTime = 300; // Using the value as debounce time by approximating average human reaction https://humanbenchmark.com/tests/reactiontime/statistics
+
+  private readonly _productService = inject(ProductService);
+
+  products: Product[] = [];
+
   searchProduct(event: SearchbarCustomEvent) {
     const searchText = event.detail.value;
   }
